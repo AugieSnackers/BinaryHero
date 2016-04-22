21286822b399f65e53a8aca908cfc56d79451243
kayleenolan12 <kayleenolan12@augustana.edu>
2016-04-21 23:23:38 -0500

    password layout

---
+++ app/src/main/java/edu/augustana/snackers/binaryhero/PasswordActivity.java
@@ -0,0 +1,24 @@
+package edu.augustana.snackers.binaryhero;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.com.example.nelly.binaryhero.R;
+
+
+/**
+ * Created by kaylee on 4/21/2016.
+ */
+public class PasswordActivity {
+
+
+    public class SettingsActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_password);
+        }
+    }
+
+}
