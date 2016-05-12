46fb98e4da044aa07b3a66f02e1633356852ba14
cmgreen <christophergreen12@augustana.edu>
2016-05-12 15:50:10 -0500

    Add blank tutorial popupwindow

--- app/src/main/java/edu/augustana/snackers/binaryhero/MainPageActivity.java
+++ app/src/main/java/edu/augustana/snackers/binaryhero/MainPageActivity.java
@@ -1,15 +1,18 @@
 package edu.augustana.snackers.binaryhero;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.PopupWindow;
 import android.widget.ToggleButton;
 import android.widget.EditText;
 
@@ -21,6 +24,8 @@ public class MainPageActivity extends AppCompatActivity {
     private ToggleButton baseSwitch;
     private Typeface myTypeface;
     private int passwordLevel = 0;
+    private View popupViewTut, popupViewAbout;
+    private PopupWindow popupWindowTut, popupWindowAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +55,7 @@ public class MainPageActivity extends AppCompatActivity {
                     }
                 }
 
-                 extras.putInt(GameArenaActivity.PLAYER_LEVEL_EXTRA, passwordLevel);
+                extras.putInt(GameArenaActivity.PLAYER_LEVEL_EXTRA, passwordLevel);
                 extras.putBoolean(GameArenaActivity.GAME_MODE_EXTRA, baseSwitch.isChecked());
                 intent.putExtras(extras);
                 startActivity(intent);
@@ -95,16 +100,53 @@ public class MainPageActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.tutorial) {
+            showTutorial();
             return true;
-        }else if(id == R.id.about){
+        } else if (id == R.id.about){
+            showAboutPage();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showTutorial() {
+        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.frameLayout2);
+        LayoutInflater layoutInflater = (LayoutInflater) getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+        popupViewTut = layoutInflater.inflate(R.layout.activity_tutorial, null);
+        popupWindowTut = new PopupWindow(popupViewTut, FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+        popupWindowTut.showAtLocation(frameLayout, Gravity.CENTER, 0, 0);
+        Button closeButton = (Button) popupViewTut.findViewById(R.id.buttonTut);
+        closeButton.setTypeface(myTypeface);
+        closeButton.setBackgroundColor(0xFF000000);
+        assert closeButton != null;
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindowTut.dismiss();
+            }
+        });
+    }
+
+    private void showAboutPage() {
+        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.frameLayout2);
+        LayoutInflater layoutInflater = (LayoutInflater) getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+        popupViewTut = layoutInflater.inflate(R.layout.activity_about, null);
+        popupWindowAbout = new PopupWindow(popupViewTut, FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+        popupWindowAbout.showAtLocation(frameLayout, Gravity.NO_GRAVITY, 500, 500);
+
+        popupViewTut.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                popupWindowAbout.dismiss();
+                return true;
+            }
+        });
     }
 
 
